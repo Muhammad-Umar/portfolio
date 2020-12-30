31a072f5546e28b9a7d02aad44dca45ccb053eb4
Muhammad Umar <umermalik784@gmail.com>
2020-12-30 06:55:12 +0100

    Home cover styles

--- src/components/HomeCoverDescription.js
+++ src/components/HomeCoverDescription.js
@@ -6,7 +6,7 @@ const HomeCoverDescription = () => {
             <span className="dash-top-line"></span>
             <h1>I'm Muhammad Umar</h1>
             <h2>Full Stack Developer</h2>
-            <h3>Expertise in <span>React</span></h3>
+            <h3>Experience in <span className="skill-name">React</span> <span className="text-cursor"></span></h3>
         </div>
     )
 }
